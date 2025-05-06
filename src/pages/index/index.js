e1d627aaaa8fed2482bded548f1b7fc2910be5ea
李成 <2414687096@qq.com>
2025-05-06 13:51:32 +0800

    修正测评页面文本样式，强调家长填写的重要性

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -272,7 +272,7 @@ export default function Index() {
             </View>
             
             <View style={{ textIndent: '2em', marginTop: '20rpx' }}>
-              该测评可以反映孩子的优势和不足，测评后的干预建议则可为您后续针对性干预提供参考。测评结果的准确性和可靠性取决于您（最熟悉孩子的家长填写）是否遵循要求认真如实完成所有作答。
+              该测评可以反映孩子的优势和不足，测评后的干预建议则可为您后续针对性干预提供参考。测评结果的准确性和可靠性取决于<Text style={{ fontWeight: 'bold' }}>您（最熟悉孩子的家长填写）</Text>是否遵循要求认真如实完成所有作答。
             </View>
 
             <View style={{fontSize:'large',fontWeight:'bolder',width:'calc(90vw - 14vw)',textAlign:'center',position:'absolute',bottom:'5%'}}> 致家长的一封信 </View>
